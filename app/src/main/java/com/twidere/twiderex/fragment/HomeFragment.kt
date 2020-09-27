be9f785bddb41b96a3451680a2bade5753c89ad8
Tlaster <tlaster@outlook.com>
2020-09-27 20:23:50 +0800

    add home drawer

--- app/src/main/java/com/twidere/twiderex/fragment/HomeFragment.kt
+++ app/src/main/java/com/twidere/twiderex/fragment/HomeFragment.kt
@@ -1,14 +1,26 @@
 package com.twidere.twiderex.fragment
 
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.Box
-import androidx.compose.foundation.Icon
-import androidx.compose.foundation.Text
-import androidx.compose.foundation.contentColor
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.savedinstancestate.savedInstanceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.viewModel
+import com.twidere.twiderex.component.NetworkImage
 import com.twidere.twiderex.component.home.*
+import com.twidere.twiderex.ui.profileImageSize
+import com.twidere.twiderex.viewmodel.twitter.ActiveAccountViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -48,13 +60,27 @@ class HomeFragment : JetFragment() {
             SearchItem(),
             MeItem(),
         )
-
+        val scaffoldState = rememberScaffoldState()
         Scaffold(
+            scaffoldState = scaffoldState,
             topBar = {
                 com.twidere.twiderex.component.AppBar(
                     title = {
                         Text(text = menus[selectedItem].name)
                     },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                if (scaffoldState.drawerState.isOpen) {
+                                    scaffoldState.drawerState.close()
+                                } else {
+                                    scaffoldState.drawerState.open()
+                                }
+                            }
+                        ) {
+                            Icon(asset = Icons.Default.Menu)
+                        }
+                    }
                 )
             },
             bottomBar = {
@@ -62,6 +88,9 @@ class HomeFragment : JetFragment() {
                     setSelectedItem(it)
                 }
             },
+            drawerContent = {
+                HomeDrawer()
+            }
         ) {
             Box(
                 paddingBottom = it.bottom,
@@ -74,5 +103,102 @@ class HomeFragment : JetFragment() {
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun HomeDrawer() {
+    val viewModel = viewModel<ActiveAccountViewModel>()
+    Column {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        ListItem(
+            icon = {
+                NetworkImage(
+                    url = viewModel.account.user.profileImage,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .width(profileImageSize)
+                        .height(profileImageSize)
+                )
+            },
+            text = {
+                Text(
+                    text = viewModel.account.user.name,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            },
+            secondaryText = {
+                Text(
+                    text = "@${viewModel.account.user.screenName}",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            },
+            trailing = {
+                IconButton(onClick = {}) {
+                    Icon(asset = Icons.Default.ArrowDropDown)
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(text = viewModel.account.user.friendsCount.toString())
+                Text(text = "Following")
+            }
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(text = viewModel.account.user.followersCount.toString())
+                Text(text = "Followers")
+            }
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(text = viewModel.account.user.listedCount.toString())
+                Text(text = "Listed")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Divider()
+
+        ScrollableColumn(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            for (it in (0 until 10)) {
+                ListItem(
+                    icon = {
+                        Icon(asset = Icons.Default.Settings)
+                    },
+                    text = {
+                        Text(text = "Settings")
+                    }
+                )
+            }
+        }
+
+        Divider()
+
+        ListItem(
+            icon = {
+                Icon(asset = Icons.Default.Settings)
+            },
+            text = {
+                Text(text = "Settings")
+            }
+        )
+
     }
 }
