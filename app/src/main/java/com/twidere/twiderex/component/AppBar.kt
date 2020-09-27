e45c4a8c8f5597247aaf3579daaef0e8755dbe8a
Tlaster <tlaster@outlook.com>
2020-09-27 17:32:53 +0800

    Add appbar

---
+++ app/src/main/java/com/twidere/twiderex/component/AppBar.kt
@@ -0,0 +1,26 @@
+package com.twidere.twiderex.component
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppBar(
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable (() -> Unit)? = null,
+    actions: @Composable RowScope.() -> Unit = {},
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    elevation: Dp = 0.dp
+) {
+    TopAppBar(
+        title, modifier, navigationIcon, actions, backgroundColor, contentColor, elevation,
+    )
+}
