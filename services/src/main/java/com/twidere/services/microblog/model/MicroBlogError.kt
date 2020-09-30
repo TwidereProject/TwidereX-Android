cd0412c195a035044db839147b519248f1043af6
Tlaster <tlaster@outlook.com>
2020-09-30 20:50:24 +0800

    Add MicroBlogError

---
+++ services/src/main/java/com/twidere/services/microblog/model/MicroBlogError.kt
@@ -0,0 +1,14 @@
+package com.twidere.services.microblog.model
+
+class MicroBlogError : Error {
+    constructor() : super()
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+    constructor(cause: Throwable?) : super(cause)
+    constructor(
+        message: String?,
+        cause: Throwable?,
+        enableSuppression: Boolean,
+        writableStackTrace: Boolean
+    ) : super(message, cause, enableSuppression, writableStackTrace)
+}
