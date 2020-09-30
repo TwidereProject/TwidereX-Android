b1726f0904f4c66f89fb3b529cebe99ffd5e45e4
Tlaster <tlaster@outlook.com>
2020-09-30 20:44:17 +0800

    Add ComposeAnnotation

---
+++ app/src/main/java/com/twidere/twiderex/annotations/ComposeAnnotation.kt
@@ -0,0 +1,5 @@
+package com.twidere.twiderex.annotations
+
+
+@RequiresOptIn("This component/layout will be upgraded/rewrite once google release new component/layout for Jetpack Compose")
+annotation class IncomingComposeUpdate
