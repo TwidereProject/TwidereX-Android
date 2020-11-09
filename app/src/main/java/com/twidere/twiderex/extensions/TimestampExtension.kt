ca36016335cb9e60ddb584b1511f22cd0c920ef7
Tlaster <tlaster@outlook.com>
2020-11-09 19:43:20 +0800

    update humanizedTimestamp

--- app/src/main/java/com/twidere/twiderex/extensions/TimestampExtension.kt
+++ app/src/main/java/com/twidere/twiderex/extensions/TimestampExtension.kt
@@ -20,31 +20,12 @@
  */
 package com.twidere.twiderex.extensions
 
-import android.content.res.Resources
-import android.os.Build
-import org.ocpsoft.prettytime.PrettyTime
-import java.text.DateFormat
-import java.util.Date
-
-private val prettyTime = PrettyTime(
-    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
-        Resources.getSystem().configuration.locales[0]
-    } else {
-        @Suppress("DEPRECATION")
-        Resources.getSystem().configuration.locale
-    }
-)
-
-private val SECOND_MILLIS = 1000
-private val MINUTE_MILLIS = 60 * SECOND_MILLIS
-private val HOUR_MILLIS = 60 * MINUTE_MILLIS
-private val DAY_MILLIS = 24 * HOUR_MILLIS
+import android.text.format.DateUtils
 
 fun Long.humanizedTimestamp(): String {
-    val date = Date(this)
-    return if (System.currentTimeMillis() - date.time > 3 * HOUR_MILLIS) {
-        DateFormat.getDateTimeInstance().format(date)
-    } else {
-        prettyTime.format(date)
-    }
+    return DateUtils.getRelativeTimeSpanString(
+        this, System.currentTimeMillis(),
+        DateUtils.MINUTE_IN_MILLIS,
+        DateUtils.FORMAT_ABBREV_ALL
+    ).toString()
 }
