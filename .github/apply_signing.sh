e08ed0299d7d4265585e7304fe5db2cd3be37f85
Tlaster <tlaster@outlook.com>
2020-11-20 17:16:35 +0800

    update signing

--- .github/apply_signing.sh
+++ .github/apply_signing.sh
@@ -1,6 +1,5 @@
-mkdir build
 echo $SIGNING_KEY | base64 -d > key.jks
-echo "storeFile=../key.jks
+echo "storeFile=key.jks
 storePassword=$KEY_STORE_PASSWORD
 keyAlias=$ALIAS
 keyPassword=$KEY_PASSWORD" >signing.properties
