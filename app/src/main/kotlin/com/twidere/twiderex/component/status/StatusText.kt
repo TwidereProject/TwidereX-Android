fde250fd50a28ffc73b080ac67505ab6dbf9d949
Tlaster <tlaster@outlook.com>
2021-02-22 16:33:07 +0800

    tweaking StatusText performance

--- app/src/main/kotlin/com/twidere/twiderex/component/status/StatusText.kt
+++ app/src/main/kotlin/com/twidere/twiderex/component/status/StatusText.kt
@@ -27,9 +27,9 @@ import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Providers
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.runtime.remember
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import com.twidere.twiderex.component.navigation.LocalNavigator
 import com.twidere.twiderex.model.ui.UiStatus
@@ -43,13 +43,10 @@ private const val TAG_URL = "url"
 @Composable
 fun StatusText(
     status: UiStatus,
-    color: Color = Color.Unspecified,
     onStatusTextClicked: () -> Unit = {},
 ) {
     val navigator = LocalNavigator.current
-    val textColor = color.takeOrElse {
-        LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
-    }
+    val textColor = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
     Providers(
         LocalTextStyle provides MaterialTheme.typography.body1.copy(color = textColor)
     ) {
@@ -91,80 +88,103 @@ private fun RenderContent(
 }
 
 @Composable
-public fun UiStatus.contentAnnotatedString(): AnnotatedString {
-    val document = Jsoup.parse(htmlText.replace("\n", "<br>"))
-    return buildAnnotatedString {
-        document.body().childNodes().forEach {
-            RenderNode(it, this@contentAnnotatedString)
+fun UiStatus.contentAnnotatedString(): AnnotatedString {
+    val textColor = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+    val textStyle = MaterialTheme.typography.body1.copy(color = textColor)
+    val linkStyle = textStyle.copy(MaterialTheme.colors.primary)
+    val styleData = remember(textStyle, linkStyle) {
+        StyleData(
+            textStyle = textStyle,
+            linkStyle = linkStyle,
+        )
+    }
+    return remember(
+        htmlText,
+        styleData,
+    ) {
+        val document = Jsoup.parse(htmlText.replace("\n", "<br>"))
+        buildAnnotatedString {
+            document.body().childNodes().forEach {
+                RenderNode(it, this@contentAnnotatedString, styleData)
+            }
         }
     }
 }
 
-@Composable
-private fun AnnotatedString.Builder.RenderNode(node: Node, status: UiStatus) {
+data class StyleData(
+    val textStyle: TextStyle,
+    val linkStyle: TextStyle,
+)
+
+private fun AnnotatedString.Builder.RenderNode(node: Node, status: UiStatus, styleData: StyleData) {
     when (node) {
         is Element -> {
-            this.RenderElement(node, status = status)
+            this.RenderElement(node, status = status, styleData = styleData)
         }
         is TextNode -> {
-            RenderText(node.text())
+            RenderText(node.text(), styleData.textStyle)
         }
     }
 }
 
-@Composable
-private fun AnnotatedString.Builder.RenderText(text: String) {
+private fun AnnotatedString.Builder.RenderText(text: String, textStyle: TextStyle) {
     pushStyle(
-        LocalTextStyle.current.toSpanStyle()
+        textStyle.toSpanStyle()
     )
     append(text)
     pop()
 }
 
-@Composable
-private fun AnnotatedString.Builder.RenderElement(element: Element, status: UiStatus) {
+private fun AnnotatedString.Builder.RenderElement(
+    element: Element,
+    status: UiStatus,
+    styleData: StyleData
+) {
     when (element.normalName()) {
         "a" -> {
-            RenderLink(element, status)
+            RenderLink(element, status, styleData)
         }
         "br" -> {
-            RenderText("\n")
+            RenderText("\n", styleData.textStyle)
         }
         "span", "p" -> {
             element.childNodes().forEach {
-                RenderNode(node = it, status = status)
+                RenderNode(node = it, status = status, styleData = styleData)
             }
         }
     }
 }
 
-@Composable
-private fun AnnotatedString.Builder.RenderLink(element: Element, status: UiStatus) {
+private fun AnnotatedString.Builder.RenderLink(
+    element: Element,
+    status: UiStatus,
+    styleData: StyleData
+) {
     val href = element.attr("href")
     val entity = status.url.firstOrNull { it.url == href }
     val media = status.media.firstOrNull { it.url == href }
-    Providers(
-        LocalTextStyle provides LocalTextStyle.current.copy(color = MaterialTheme.colors.primary)
-    ) {
-        when {
-            entity != null -> {
-                if (!entity.displayUrl.contains("pic.twitter.com") &&
-                    !(status.quote != null && entity.expandedUrl.endsWith(status.quote.statusId))
-                ) {
-                    pushStringAnnotation(TAG_URL, entity.expandedUrl)
-                    RenderText(entity.displayUrl)
-                    pop()
-                }
-            }
-            media != null -> {
-            }
-            else -> {
-                pushStringAnnotation(TAG_URL, href)
-                element.childNodes().forEach {
-                    RenderNode(node = it, status = status)
-                }
+    when {
+        entity != null -> {
+            if (!entity.displayUrl.contains("pic.twitter.com") &&
+                !(status.quote != null && entity.expandedUrl.endsWith(status.quote.statusId))
+            ) {
+                pushStringAnnotation(TAG_URL, entity.expandedUrl)
+                RenderText(entity.displayUrl, styleData.linkStyle)
                 pop()
             }
+        }
+        media != null -> {
+        }
+        else -> {
+            pushStringAnnotation(TAG_URL, href)
+            element.childNodes().forEach {
+                RenderNode(
+                    node = it,
+                    status = status,
+                    styleData = styleData.copy(textStyle = styleData.linkStyle)
+                )
+            }
+            pop()
         }
     }
 }
