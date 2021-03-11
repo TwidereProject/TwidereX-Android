04e929c647e855c278f65b3309254bd6b6fd999d
Tlaster <tlaster@outlook.com>
2021-03-11 12:15:46 +0800

    Revert "remove rememberSaveable workaround"

--- app/src/main/kotlin/com/twidere/twiderex/component/status/StatusText.kt
+++ app/src/main/kotlin/com/twidere/twiderex/component/status/StatusText.kt
@@ -39,6 +39,7 @@ import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
