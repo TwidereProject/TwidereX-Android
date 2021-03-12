9d2721bd3d4a781328ae02aed4959f94e4a94aa2
Tlaster <tlaster@outlook.com>
2021-03-12 10:19:20 +0800

    upgrade ksp to 1.4.30-1.0.0-alpha05

--- assisted-processor/build.gradle.kts
+++ assisted-processor/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.4.30-1.0.0-alpha04")
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.4.30-1.0.0-alpha05")
 }
 
 sourceSets.main {
