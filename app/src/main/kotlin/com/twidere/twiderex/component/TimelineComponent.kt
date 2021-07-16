f9f7d865cc0026ba6e36668db631772ce5903fb4
itsMimao <qiaoxiaoxi621@gmail.com>
2021-07-16 14:30:25 +0800

    opt onRefresh in SwipeToRefreshLayzout

--- app/src/main/kotlin/com/twidere/twiderex/component/TimelineComponent.kt
+++ app/src/main/kotlin/com/twidere/twiderex/component/TimelineComponent.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
@@ -48,7 +47,7 @@ fun TimelineComponent(
     contentPadding: PaddingValues = PaddingValues(0.dp),
     lazyListController: LazyListController? = null,
 ) {
-    val items by rememberUpdatedState(newValue = viewModel.source.collectAsLazyPagingItems())
+    val items = viewModel.source.collectAsLazyPagingItems()
     val loadingBetween by viewModel.loadingBetween.observeAsState(initial = listOf())
     SwipeToRefreshLayout(
         refreshingState = items.loadState.refresh is LoadState.Loading,
