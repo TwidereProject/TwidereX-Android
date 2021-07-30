f865e2980ece6fcdac3f8df11e63ae0c68362101
Tlaster <tlaster@outlook.com>
2021-07-30 10:47:26 +0800

    version 1.5.0-beta02

--- buildSrc/src/main/kotlin/Package.kt
+++ buildSrc/src/main/kotlin/Package.kt
@@ -2,6 +2,6 @@ object Package {
     const val group = "com.twidere"
     const val name = "TwidereX"
     const val id = "$group.twiderex"
-    const val versionName = "1.5.0-beta01"
-    const val versionCode = 50
+    const val versionName = "1.5.0-beta02"
+    const val versionCode = 51
 }
