f539539510ccf88bb2797e6a093a6786048d814c
Tlaster <tlaster@outlook.com>
2021-08-06 15:22:40 +0800

    fix koin context

--- common/src/commonMain/kotlin/com/twidere/twiderex/di/ext/ComposeExt.kt
+++ common/src/commonMain/kotlin/com/twidere/twiderex/di/ext/ComposeExt.kt
@@ -23,19 +23,19 @@ package com.twidere.twiderex.di.ext
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import org.koin.core.Koin
-import org.koin.core.context.GlobalContext
 import org.koin.core.parameter.ParametersDefinition
 import org.koin.core.qualifier.Qualifier
+import org.koin.mp.KoinPlatformTools
 
 @Composable
 inline fun <reified T> get(
     qualifier: Qualifier? = null,
     noinline parameters: ParametersDefinition? = null,
 ): T = remember(qualifier, parameters) {
-    GlobalContext.get().get(qualifier, parameters)
+    KoinPlatformTools.defaultContext().get().get(qualifier, parameters)
 }
 
 @Composable
 fun getKoin(): Koin = remember {
-    GlobalContext.get()
+    KoinPlatformTools.defaultContext().get()
 }
