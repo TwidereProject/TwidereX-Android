e1a86e5a94d0b93ca9dd9bfd13a235eac9cfb49b
Tlaster <tlaster@outlook.com>
2021-08-06 17:20:30 +0800

    fix rtl language display

--- app/src/main/kotlin/com/twidere/twiderex/component/status/StatusText.kt
+++ app/src/main/kotlin/com/twidere/twiderex/component/status/StatusText.kt
@@ -29,6 +29,7 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
@@ -36,17 +37,22 @@ import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.twidere.twiderex.R
 import com.twidere.twiderex.model.enums.PlatformType
 import com.twidere.twiderex.model.ui.UiStatus
+import java.text.Bidi
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -84,13 +90,25 @@ fun ColumnScope.StatusText(
     }
     AnimatedVisibility(visible = expanded) {
         Column {
-            HtmlText(
-                htmlText = status.htmlText,
-                maxLines = maxLines,
-                linkResolver = { href ->
-                    status.resolveLink(href)
-                },
-            )
+            val bidi = remember(status.rawText) {
+                Bidi(status.rawText, Bidi.DIRECTION_DEFAULT_LEFT_TO_RIGHT)
+            }
+            CompositionLocalProvider(
+                LocalLayoutDirection provides if (bidi.baseIsLeftToRight()) {
+                    LayoutDirection.Ltr
+                } else {
+                    LayoutDirection.Rtl
+                }
+            ) {
+                HtmlText(
+                    modifier = Modifier.fillMaxWidth(),
+                    htmlText = status.htmlText,
+                    maxLines = maxLines,
+                    linkResolver = { href ->
+                        status.resolveLink(href)
+                    },
+                )
+            }
 
             if (showMastodonPoll && status.platformType == PlatformType.Mastodon && status.poll != null) {
                 Spacer(modifier = Modifier.height(StatusTextDefaults.Mastodon.PollSpacing))
