43a9a4ab575be77930c7a782027b20b8836b5910
Tlaster <tlaster@outlook.com>
2021-08-27 17:00:25 +0800

    move Event to common

--- common/src/commonMain/kotlin/com/twidere/twiderex/utils/Event.kt
+++ common/src/commonMain/kotlin/com/twidere/twiderex/utils/Event.kt
@@ -20,8 +20,6 @@
  */
 package com.twidere.twiderex.utils
 
-import androidx.lifecycle.Observer
-
 /**
  * Used as a wrapper for data that is exposed via a Flow that represents an event.
  */
@@ -47,18 +45,4 @@ open class Event<out T>(private val content: T) {
      * Returns the content, even if it's already been handled.
      */
     fun peekContent(): T = content
-}
-
-/**
- * An [Observer] for [Event]s, simplifying the pattern of checking if the [Event]'s content has
- * already been handled.
- *
- * [onEventUnhandledContent] is *only* called if the [Event]'s contents has not been handled.
- */
-class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
-    override fun onChanged(event: Event<T>?) {
-        event?.getContentIfNotHandled()?.let {
-            onEventUnhandledContent(it)
-        }
-    }
 }
