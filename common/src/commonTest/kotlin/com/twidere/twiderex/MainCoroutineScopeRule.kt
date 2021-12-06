ac7225f9a1ae8a20d7222765df23d6ffb117aec3
itsMimao <qiaoxiaoxi621@gmail.com>
2021-12-06 12:11:35 +0800

    format code

--- common/src/commonTest/kotlin/com/twidere/twiderex/MainCoroutineScopeRule.kt
+++ common/src/commonTest/kotlin/com/twidere/twiderex/MainCoroutineScopeRule.kt
@@ -1,3 +1,23 @@
+/*
+ *  Twidere X
+ *
+ *  Copyright (C) 2020-2021 Tlaster <tlaster@outlook.com>
+ * 
+ *  This file is part of Twidere X.
+ * 
+ *  Twidere X is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  Twidere X is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with Twidere X. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.twidere.twiderex
 
 import kotlinx.coroutines.Dispatchers
@@ -15,12 +35,6 @@ class MainCoroutineScopeRule(private val dispatcher: TestCoroutineDispatcher = T
     TestCoroutineScope by TestCoroutineScope() {
     override fun starting(description: Description?) {
         super.starting(description)
-        // If your codebase allows the injection of other dispatchers like
-        // Dispatchers.Default and Dispatchers.IO, consider injecting all of them here
-        // and renaming this class to `CoroutineScopeRule`
-        //
-        // All injected dispatchers in a test should point to a single instance of
-        // TestCoroutineDispatcher.
         Dispatchers.setMain(dispatcher)
     }
 
