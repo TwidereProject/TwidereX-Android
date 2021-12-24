9f95260baed952f2ca71e43fffbb36af596ce85f
Tlaster <tlaster@outlook.com>
2021-12-24 22:45:12 +0800

    fix signing

--- .github/apply_signing.sh
+++ .github/apply_signing.sh
@@ -1,5 +1,5 @@
 echo $SIGNING_KEY | base64 -d > key.jks
-echo $GOOGLE_SERVICES | base64 -d > app/google-services.json
+echo $GOOGLE_SERVICES | base64 -d > android/google-services.json
 echo "storeFile=key.jks
 storePassword=$KEY_STORE_PASSWORD
 keyAlias=$ALIAS
