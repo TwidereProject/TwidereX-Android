9f9720a86cd2688844c8465a108304a7849e6d71
Tlaster <tlaster@outlook.com>
2021-12-25 11:03:52 +0800

    update desktop packages

--- buildSrc/src/main/kotlin/Package.kt
+++ buildSrc/src/main/kotlin/Package.kt
@@ -2,6 +2,16 @@ object Package {
     const val group = "com.twidere"
     const val name = "Twidere X"
     const val id = "$group.twiderex"
-    const val versionName = "1.6.0-dev01"
-    const val versionCode = 56
+    val versionName =
+        "${Version.main}.${Version.mirror}.${Version.patch}${if (Version.revision.isNotEmpty()) "-${Version.revision}" else ""}"
+    const val copyright = "Copyright (C) TwidereProject and Contributors"
+    const val versionCode = Version.build
+
+    object Version {
+        const val main = "1"
+        const val mirror = "6"
+        const val patch = "0"
+        const val revision = "dev01"
+        const val build = 56
+    }
 }
