7a1d2350cedfe752e8abc9c5025d9e2cbef84a5a
huixing <wanghuixing24@gmail.com>
2022-04-06 21:12:16 +0800

    add debugTool (currently only working on android)

---
+++ common/src/commonMain/kotlin/com/twidere/twiderex/navigation/ComposeDebugTool.kt
@@ -0,0 +1,80 @@
+/*
+ *  Twidere X
+ *
+ *  Copyright (C) TwidereProject and Contributors
+ * 
+ *  This file is part of Twidere X.
+ * 
+ *  Twidere X is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  Twidere X is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with Twidere X. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.twidere.twiderex.navigation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import moe.tlaster.precompose.navigation.NavController
+import moe.tlaster.precompose.navigation.currentBackStackEntryAsState
+
+@Composable
+fun ComposeDebugTool(
+    rootNavController: NavController,
+) {
+
+    var showDebug by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(Unit) {
+        delay(3000)
+        showDebug = true
+    }
+
+    if (!showDebug) {
+        return
+    }
+    val state by rootNavController.currentBackStackEntryAsState()!!
+    var debugOpen by remember {
+        mutableStateOf(false)
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Button(
+            modifier = Modifier.padding(top = 56.dp),
+            onClick = {
+                debugOpen = !debugOpen
+            }
+        ) {
+            Text("Debug")
+        }
+        if (debugOpen) {
+            Text(
+                modifier = Modifier.background(MaterialTheme.colors.surface),
+                text = state?.route?.route ?: "UnKnow route",
+                color = MaterialTheme.colors.primary
+            )
+        }
+    }
+}
