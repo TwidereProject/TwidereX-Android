6a37c22625647de93ad884908a6a7107df4fc1ef
seiko <seiko_des@outlook.com>
2023-01-08 14:20:12 +0800

    fix fontSize to dp

--- common/src/commonMain/kotlin/com/twidere/twiderex/component/status/DetailedStatusComponent.kt
+++ common/src/commonMain/kotlin/com/twidere/twiderex/component/status/DetailedStatusComponent.kt
@@ -2,19 +2,19 @@
  *  Twidere X
  *
  *  Copyright (C) TwidereProject and Contributors
- * 
+ *
  *  This file is part of Twidere X.
- * 
+ *
  *  Twidere X is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  Twidere X is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with Twidere X. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -43,6 +43,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.twidere.twiderex.component.FormattedTime
 import com.twidere.twiderex.component.painterResource
@@ -210,9 +211,12 @@ private fun StatusStatistics(
   icon: Painter,
   contentDescription: String?,
 ) {
+  val iconSize = with(LocalDensity.current) {
+    MaterialTheme.typography.body1.fontSize.toDp()
+  }
   Row {
     Icon(
-      modifier = Modifier.size(MaterialTheme.typography.body1.fontSize.value.dp),
+      modifier = Modifier.size(iconSize),
       painter = icon,
       contentDescription = contentDescription,
     )
