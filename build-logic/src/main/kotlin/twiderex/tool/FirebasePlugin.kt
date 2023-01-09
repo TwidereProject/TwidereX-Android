7f8268927332eab494a104c5ef5112826a99ef20
seiko <seiko_des@outlook.com>
2023-01-09 19:02:04 +0800

    remove firebase appdistribution

--- build-logic/src/main/kotlin/twiderex/tool/FirebasePlugin.kt
+++ build-logic/src/main/kotlin/twiderex/tool/FirebasePlugin.kt
@@ -3,8 +3,6 @@ package twiderex.tool
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
-import com.google.firebase.appdistribution.gradle.firebaseAppDistribution
-import twiderex.androidApplication
 import twiderex.implementation
 
 @Suppress("unused")
@@ -14,9 +12,6 @@ class FirebasePlugin : Plugin<Project> {
       with(pluginManager) {
         apply("com.google.gms.google-services")
         apply("com.google.firebase.crashlytics")
-        if (appDistributeJsonExits) {
-          apply("com.google.firebase.appdistribution")
-        }
       }
       dependencies {
         implementation(platform("com.google.firebase:firebase-bom:30.4.1"))
@@ -24,22 +19,6 @@ class FirebasePlugin : Plugin<Project> {
         implementation("com.google.firebase:firebase-crashlytics-ktx")
         implementation("com.google.firebase:firebase-appdistribution-api-ktx:16.0.0-beta04")
       }
-      androidApplication {
-        buildTypes {
-          release {
-            if (appDistributeJsonExits) {
-              firebaseAppDistribution {
-                serviceCredentialsFile = file("firebase-app-distribute.json").absolutePath
-                artifactType = "APK"
-                groups = "internal-tester"
-              }
-            }
-          }
-        }
-      }
     }
   }
-
-  private val Project.appDistributeJsonExits: Boolean
-    get() = file("firebase-app-distribute.json").exists()
 }
