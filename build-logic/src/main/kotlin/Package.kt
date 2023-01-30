37cb21025986bb7e9be0dadfb7849d597d4ae7ef
Tlaster <tlaster@outlook.com>
2023-01-30 14:19:19 +0800

    version 1.7.0-beta02

--- build-logic/src/main/kotlin/Package.kt
+++ build-logic/src/main/kotlin/Package.kt
@@ -11,7 +11,7 @@ object Package {
         const val main = "1"
         const val mirror = "7"
         const val patch = "0"
-        const val revision = "beta01"
-        const val build = 60
+        const val revision = "beta02"
+        const val build = 61
     }
 }
