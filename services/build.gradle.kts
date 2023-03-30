3185869eaaaaa3c56b21c2ec5c74fd6f9ceda9b8
seiko <seiko_des@outlook.com>
2023-03-30 12:45:23 +0800

    fix build

--- services/build.gradle.kts
+++ services/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
                 implementation(libs.bundles.reftrofit2)
                 implementation(libs.bundles.ktor)
                 implementation(libs.square.okhttp)
+                implementation(libs.square.okhttp.logging.interceptor)
                 implementation("com.github.Tlaster:Hson:0.1.4")
             }
         }
